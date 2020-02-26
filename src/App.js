33ef94c3fe95423c79fc77cdb82cddf7494d22d3
Michael_Vano <michaelv@dev6.com>
2020-02-26 14:52:36 -0500

    step 5-1: convert App functional component to a class component

--- src/App.js
+++ src/App.js
@@ -15,26 +15,33 @@ function ExampleNotificationHeader() {
   );
 }
 
-function App() {
-  return (
-    // Step 4-3: Adding a fragment, an empty "<> </>" tag, because React only allows a single parent element in the return/render function.
-    // If we did not put a Fragment tag, <Header> and <Notification> would both be the top level elements in this component, and that is not allowed
-    // The fragment tag will not get rendered.
-    // Use the fragment to avoid excessive use of <div> tags just to wrap elements.
-    <>
-      {/* // step 3-1: Add properties "name" and "onButtonClick", passing a string into "name" and a function into "onButtonClick" */}
-      <Header name="John Deere Training" onButtonClick={(e) => alert('Button Clicked')} />
-      {/*
+// Step 5-1: Whenever dealing with state management, we MUST use class component, since functional components are just presentational
+// so converted the App component into a class component
+// all class components MUST extend "React.component"
+class App extends React.Component  {
+
+  // Step 5-1: When using class components, we MUST call the "render" method and return the HTML code to be displayed
+  render() {
+    return (
+      // Step 4-3: Adding a fragment, an empty "<> </>" tag, because React only allows a single parent element in the return/render function.
+      // If we did not put a Fragment tag, <Header> and <Notification> would both be the top level elements in this component, and that is not allowed
+      // The fragment tag will not get rendered.
+      // Use the fragment to avoid excessive use of <div> tags just to wrap elements.
+      <>
+        {/* // step 3-1: Add properties "name" and "onButtonClick", passing a string into "name" and a function into "onButtonClick" */}
+        <Header name="John Deere Training" onButtonClick={(e) => alert('Button Clicked')} />
+        {/*
       Step 4-3: Added the Notification component, passing in the "ExampleNotificationHeader" component to the "header" prop.
       Also set the "notificationStyle" prop to use the "notificationStyles.info" styles.
       */}
-      <Notification header={<ExampleNotificationHeader />} notificationStyle={notificationStyles.info}>
-        {/* Step 4-3: Any content placed between the <Notification> tags, will be passed down into the component in the "props.children" property */}
-        <p>Notification Custom Content</p>
-        <p>More Custom Content</p>
-      </Notification>
-    </>
-  );
+        <Notification header={<ExampleNotificationHeader />} notificationStyle={notificationStyles.info}>
+          {/* Step 4-3: Any content placed between the <Notification> tags, will be passed down into the component in the "props.children" property */}
+          <p>Notification Custom Content</p>
+          <p>More Custom Content</p>
+        </Notification>
+      </>
+    );
+  }
 }
 
 export default App;
