1caa3b2b04da6a60fddea9682e5fd2bc1c3a54a0
Michael_Vano <michaelv@dev6.com>
2020-02-26 10:30:59 -0500

    step 2-4: create objects with styling properties and utilize them

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,24 @@
 // Step 1-3: Every component will need to import React from the 'react' library
 import React from 'react';
 
+// Step 2-4: Create some javascript objects to be used as as styles for HTML components.
+// These can be moved to a separate file for all components to access. Just need to "export" them
+// keeping in mind that css properties need to be declared with camelCase
+const specialButtonStyle = {
+  width: '1.5rem',
+  height: '1.5rem',
+  borderRadius: '50%',
+  backgroundColor: '#fff'
+};
+
+const insideButtonCircle = {
+  width: '0.5rem',
+  height: '0.5rem',
+  borderRadius: '50%',
+  backgroundColor: '#333'
+};
+
+
 
 // Step 1-3: Create a "functional" component called "Header" that includes a title and a button
 export function Header() {
@@ -14,8 +32,10 @@ export function Header() {
         John Deere Training
       </div>
       <div>
-        <button>
-          User
+        {/* step 2-4: Pass in the "specialButtonStyle" const declared in the top of the file to add those styles to this element */}
+        <button style={specialButtonStyle}>
+          {/* step 2-4: Use the const "insideButtonCircle" to make this div inside the button into a circle */}
+          <div style={insideButtonCircle}> </div>
         </button>
       </div>
     </header>
