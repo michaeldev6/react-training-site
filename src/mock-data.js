56404442c83216052d7011d9e00e87901df6594c
Michael_Vano <michaelv@dev6.com>
2020-02-28 17:03:52 -0500

    step 9-8: reverted the route back to the correct path

--- src/mock-data.js
+++ src/mock-data.js
@@ -6,8 +6,7 @@ export function fetchUserData() {
   // It uses a promise to make a request to the resource data.
   // In this case, we are hitting an API that is used as mock data.
   // this method is not compatible with Internet Explorer
-  // Step 9-7: Intentionally mess up the route used so that we can see the ErrorListBoundary component in action
-  return fetch('https://jsonplaceholder.typicode.com/somerandomroutethatdoesntexist')
+  return fetch('https://jsonplaceholder.typicode.com/users')
     // Step 8-1: regardless of the response status code, fetch will always "resolve", but will simply set the "ok" status to false if there was an error
     // Meaning, if we wanted to check for 404 or 500 errors, we'd do check in the "then" callback function
     .then((response) => {
