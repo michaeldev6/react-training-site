28677ad0ecce476ec16d26d7fd4e6c880e2bfe79
Michael_Vano <michaelv@dev6.com>
2020-02-28 08:32:38 -0500

    step 7-3: map data to html elements and render list out

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -8,10 +8,27 @@ export function UserList(props) {
   // Step 7-2: Import in the MOCK_USER_DATA and create a copy of it to use in this component
   const list = MOCK_USER_DATA.slice();
 
+  // Step 7-3: Taking the list of data, we will "map" each of the values in the list to be an HTML element, storing it in another variable, "userList"
+  const userList = list.map((data) => {
+    return (
+      // Step 7-3: Whenever mapping a list, the "key" property will need to be added to help React identify this unique element.
+      // By default, it will use the index, but if the data is ever sorted, then the index would no longer be accurate, so a unique id
+      // is always best
+      <div key={data.id} className="user">
+        {/* Step 7-3: Display the information. Can do optional checks to confirm if the data exists or not and render the information accordingly */}
+        <div>Name: {data.name}</div>
+        <div>Email: {data.email}</div>
+        <div>Phone: {data.phone}</div>
+        <div>Website: {data.website}</div>
+      </div>
+    );
+  });
+
   // Step 7-2: Return the HTML to be rendered
   return (
     <div className="user-list">
-      User List here
+      {/* Step 7-3: Render the "userList" variable here */}
+      {userList}
     </div>
   );
 }
