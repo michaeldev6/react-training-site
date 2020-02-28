4934a9a00abe878f1adadbe073a0e161e85b9b71
Michael_Vano <michaelv@dev6.com>
2020-02-28 16:56:45 -0500

    step 9-5: update fetchUserData method to handle error better

--- src/mock-data.js
+++ src/mock-data.js
@@ -10,12 +10,26 @@ export function fetchUserData() {
     // Step 8-1: regardless of the response status code, fetch will always "resolve", but will simply set the "ok" status to false if there was an error
     // Meaning, if we wanted to check for 404 or 500 errors, we'd do check in the "then" callback function
     .then((response) => {
+      // Step 9-5: Because an error, like 404 or 500 will still resolve, we need to check here if there were any issues.
+      // if there is, we throw an Error with an appropriate message
+      console.log('Response: ', response);
+      if (!response.ok) {
+        switch (response.status) {
+          case 404:
+            throw new Error('Specified route cannot be found');
+          case 500:
+            throw new Error('Server error');
+          default:
+            throw new Error('There was an issue with the request made');
+        }
+      }
       return response.json();
     })
     // Step 8-1: The only time this would get hit is if the response fails to complete due to internet connectivity
     // or anything else that prevented the request from completing
     .catch((e) => {
-      console.error(e);
+      // Step 9-5: Update this catch to throw an error if there is an issue with the response
+      throw e;
     });
 }
 
